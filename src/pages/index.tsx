7644a3d8cb532ce60aaf93ea22996ea4826815c5
cslee0034 <cslee0034@gmail.com>
2024-08-16 10:09:22 +0900

    feat: 레이아웃 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import Header from "../components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main>Hello</main>;
+  return (
+    <main>
+      <Header />
+    </main>
+  );
 }
