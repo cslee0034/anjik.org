0330190b47fff41ceb53275a37738eb456f718e5
cslee0034 <cslee0034@gmail.com>
2024-08-16 10:10:02 +0900

    feat: 레이아웃 추가

---
+++ src/components/header.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import Sidebar from "./sidebar";
+import { useUserStore } from "../stores/useUserStore";
+
+export default function Header() {
+  const { isLoggedIn } = useUserStore();
+
+  return (
+    <header className="top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+      <nav className="flex">
+        <Link href="/" className="flex items-center w-32">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="nonzero"
+            className="w-6 h-6"
+          >
+            <path d="M15.75 8.25a.75.75 0 0 1 .75.75c0 1.12-.492 2.126-1.27 2.812a.75.75 0 1 1-.992-1.124A2.243 2.243 0 0 0 15 9a.75.75 0 0 1 .75-.75Z" />
+            <path
+              fillRule="evenodd"
+              d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM4.575 15.6a8.25 8.25 0 0 0 9.348 4.425 1.966 1.966 0 0 0-1.84-1.275.983.983 0 0 1-.97-.822l-.073-.437c-.094-.565.25-1.11.8-1.267l.99-.282c.427-.123.783-.418.982-.816l.036-.073a1.453 1.453 0 0 1 2.328-.377L16.5 15h.628a2.25 2.25 0 0 1 1.983 1.186 8.25 8.25 0 0 0-6.345-12.4c.044.262.18.503.389.676l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 0 1-1.161.886l-.143.048a1.107 1.107 0 0 0-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 0 1-1.652.928l-.679-.906a1.125 1.125 0 0 0-1.906.172L4.575 15.6Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <p className="text-xl pl-1">anjik.org</p>
+        </Link>
+        <div className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 px-4">
+          <Link
+            href="#"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            Home
+          </Link>
+          <Link
+            href="#"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            About
+          </Link>
+        </div>
+      </nav>
+      <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+        <div className="ml-auto flex-1 sm:flex-initial" />
+        <Sidebar isLoggedIn={isLoggedIn} />
+      </div>
+    </header>
+  );
+}
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,102 @@
+import Link from "next/link";
+import { CircleUser, Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useUserStore } from "../stores/useUserStore";
+
+export default function Sidebar({ isLoggedIn }: { isLoggedIn: boolean }) {
+  const userIfo = useUserStore((state) => state.userInfo);
+
+  return (
+    <div>
+      {isLoggedIn ? (
+        <div className="hidden md:flex align-middle">
+          <p className="flex items-center justify-center mx-2">
+            {userIfo?.firstName}
+          </p>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      ) : (
+        <div className="hidden md:flex">
+          <Button asChild variant="secondary" className="mx-1">
+            <Link href="/login">Login</Link>
+          </Button>
+        </div>
+      )}
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+            <Menu className="h-5 w-5" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <nav className="grid gap-6 text-lg font-medium">
+            <SheetClose asChild>
+              <Link
+                href="/"
+                className="text-muted-foreground hover:text-foreground"
+              >
+                Home
+              </Link>
+            </SheetClose>
+            <SheetClose asChild>
+              <Link
+                href="/"
+                className="text-muted-foreground hover:text-foreground"
+              >
+                About
+              </Link>
+            </SheetClose>
+            <div />
+            {isLoggedIn ? (
+              <>
+                <SheetClose asChild>
+                  <Button asChild className="bg-black">
+                    <Link href="/my-account">My Account</Link>
+                  </Button>
+                </SheetClose>
+                <Button asChild variant="outline">
+                  <Link href="/logout">Logout</Link>
+                </Button>
+              </>
+            ) : (
+              <>
+                <SheetClose asChild>
+                  <Button asChild variant="outline" className="mx-1">
+                    <Link href="/login">Login</Link>
+                  </Button>
+                </SheetClose>
+              </>
+            )}
+          </nav>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+}
