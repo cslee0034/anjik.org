8d7f09ab1a47f6ea27aee4e430aeb0e0190b300d
cslee0034 <cslee0034@gmail.com>
2024-08-18 22:31:18 +0900

    fix: mongodb 재연결 방지

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
 
 const mongoConnect = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
   try {
     await mongoose.connect(process.env.MONGODB_URI as string);
     console.log("MongoDB connected");
   } catch (error) {
-    console.error(error);
+    console.error("Error connecting to MongoDB:", error);
   }
 };
 
