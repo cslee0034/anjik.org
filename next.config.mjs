4ce1b852785e86a140df5ef263802d9210282824
cslee0034 <cslee0034@gmail.com>
2024-08-18 22:30:42 +0900

    feat: s3 주소 허용

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com", "plus.unsplash.com"],
+    domains: [
+      "images.unsplash.com",
+      "plus.unsplash.com",
+      "anjik-public.s3.ap-northeast-2.amazonaws.com",
+    ],
   },
 };
 
