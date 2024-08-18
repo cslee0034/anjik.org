eb62c4cc692a3cac3a841d9332607946bcc7833e
cslee0034 <cslee0034@gmail.com>
2024-08-19 00:51:10 +0900

    fix: ProductPage에서 Product로 이름 변경

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { Skeleton } from "@/components/ui/skeleton";
 import Subscription from "../../components/subscription";
 
-export default function ProductPage() {
+export default function Product() {
   const router = useRouter();
   const { id } = router.query;
 
